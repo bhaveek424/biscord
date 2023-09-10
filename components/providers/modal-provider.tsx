5815682f5c7db579f7da040b2672844ec211c468
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-09-10 21:47:58 +0530

    delete functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,6 +8,7 @@ import { MembersModal } from '../modals/members-modal';
 import { CreateChannelModal } from '../modals/create-channel-modal';
 import { DeleteServerModal } from '../modals/delete-server-modal';
 import { LeaveServerModal } from '../modals/leave-server-modal';
+import { DeleteChannelModal } from '../modals/delete-channel-modal';
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -26,6 +27,7 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
     </>
   );
 };
--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,6 +5,7 @@ import { Channel, ChannelType, MemberRole, Server } from '@prisma/client';
 import { Edit, Hash, Lock, Mic, Trash, Video } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import { ActionTooltip } from '../action-tooltip';
+import { useModal } from '@/hooks/use-modal-store';
 
 type ServerChannelProps = {
   channel: Channel;
@@ -25,6 +26,7 @@ export const ServerChannel = ({
 }: ServerChannelProps) => {
   const params = useParams();
   const router = useRouter();
+  const { onOpen } = useModal();
 
   const Icon = iconMap[channel.type];
 
@@ -51,7 +53,10 @@ export const ServerChannel = ({
           </ActionTooltip>
 
           <ActionTooltip label="Delete">
-            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Trash
+              onClick={() => onOpen('deleteChannel', { server, channel })}
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
