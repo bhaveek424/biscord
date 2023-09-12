0014540bdf8600772126d964e4584b78caa3c7a6
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-09-12 15:06:45 +0530

    chat item component

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -6,6 +6,10 @@ import { ChatWelcome } from './chat-welcome';
 import { useChatQuery } from '@/hooks/use-chat-query';
 import { Loader2, ServerCrash } from 'lucide-react';
 import { Fragment } from 'react';
+import { ChatItem } from './chat-item';
+import { format } from 'date-fns';
+
+const DATE_FORMAT = 'd MMM yyy, HH:mm';
 
 type ChatMessagesProps = {
   name: string;
@@ -76,7 +80,19 @@ export const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>{message.content}</div>
+              <ChatItem
+                key={message.id}
+                currentMember={member}
+                member={message.member}
+                id={message.id}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
