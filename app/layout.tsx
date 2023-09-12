5d9ffeb6ca0f18d9fc4166e74d700cb7279b6aaf
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-09-12 11:16:50 +0530

    chat message component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider';
 import { cn } from '@/lib/utils';
 import { ModalProvider } from '@/components/providers/modal-provider';
 import { SocketProvider } from '@/components/providers/socket-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -30,7 +31,7 @@ export default function RootLayout({
             storageKey="discord-theme">
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
